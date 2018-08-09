318273defe17a2d4486f2c9b32c7c0873c62be05
Benoit Lemaitre <lemaitre.b.perso@gmail.com>
2018-08-09 08:25:05 +0200

    Merlin update

--- web/refining.php
+++ web/refining.php
@@ -13,22 +13,21 @@ require_once(__DIR__."/../resources/Autoloader.php");
 $rarities = (isset($_GET['rarity']) && $_GET['rarity'] >= 0 && $_GET['rarity'] <= 3) ? [$_GET['rarity']] : [0, 1, 2, 3] ;
 $focus = isset($_GET['focus']) ? true : false ;
 $taxe = isset($_GET['taxe']) ? $_GET['taxe'] : 22;
-$city = isset($_GET['city']) ? $_GET['city'] : "Caerleon";
 
 // In order to get T4.X refining profit, we always need T3.0 prices
-$resourcesPrices = Resources::getResourcesLatestPrices($city, [2, 3, 4, 5, 6, 7, 8], array_merge([0], $rarities));
+$resourcesPrices = Resources::getResourcesLatestPrices("Caerleon", [2, 3, 4, 5, 6, 7, 8], array_merge([0], $rarities));
 $refiningProfits = Resources::getResourcesRefiningProfit([2, 3, 4, 5, 6, 7, 8], $rarities, $resourcesPrices, $taxe, $focus);
 
 if (isset($_GET['json'])) {
 	print_r(json_encode($refiningProfits));
 } else {
 	echo 'Usage :<br/>
-	noJson : Print this page<br/>
 	rarity=X : Specifie a rarity.<br/>
 	focus : With focus. Default is without<br/>
 	taxe=XX : Refining building taxe. Default is 22%<br/>
-	city=XXXX : XXXX can be : Thetford, Lymhurst, Bridgewatch, Martlock, Caerleon, FortSterling. Default is Caerleon<br/>
-	Example : https://saltan.pouicou.fr/refining.php?noJson&rarity=2&focus&city=Martlock<br/>
+	Example : https://saltan.pouicou.fr/refining.php?rarity=2&focus&taxe=12<br/>
+	<br/>
+	Prices and refining bonus based on Caerleon city<br/>
 	<br/>
 	Data based on Albion Data Client : https://github.com/broderickhyman/albiondata-client';
 	SimpleFront::printRecursiveArray('Refining profit finder', ['Item', 'Tier', 'Rarity', 'Raw resource', 'T-1 refined resource', 'Selling price', 'Taxe', 'Profit', 'Profitable ?'], $refiningProfits);
